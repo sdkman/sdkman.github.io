472b4d7a4fdecb2427352490cec752d85792ad3b
Sviatoslav <sviat.mak@gmail.com>
2024-08-09 23:28:05 +0200

    add: social links

---
+++ src/data/social-links.ts
@@ -0,0 +1,37 @@
+import {
+  FaGithub,
+  FaDiscord,
+  FaSlack,
+  FaXTwitter,
+  FaStackOverflow,
+} from 'react-icons/fa6';
+
+const socialLinks = [
+  {
+    icon: FaGithub,
+    href: 'https://github.com/sdkman',
+    alt: 'GitHub',
+  },
+  {
+    icon: FaDiscord,
+    href: 'https://discord.gg/y9mVJYVyu4',
+    alt: 'Discord',
+  },
+  {
+    icon: FaSlack,
+    href: 'https://sdkman.slack.com/',
+    alt: 'Slack',
+  },
+  {
+    icon: FaXTwitter,
+    href: 'https://x.com/sdkman_',
+    alt: 'X',
+  },
+  {
+    icon: FaStackOverflow,
+    href: 'http://stackoverflow.com/questions/tagged/sdkman',
+    alt: 'Stack Overflow',
+  },
+];
+
+export default socialLinks;
