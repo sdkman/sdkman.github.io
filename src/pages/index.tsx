be092aa892b23aeb344eb23bbf062cd2a6317806
Sviatoslav <sviat.mak@gmail.com>
2024-08-09 22:57:33 +0200

    update: welcome page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,21 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
+import Hero from '@site/src/components/sections/Hero';
+import Features from '@site/src/components/sections/Features';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+export default function WelcomePage() {
+  const { siteConfig } = useDocusaurusContext();
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />"
+    >
+      <Hero />
+
       <main>
-        <HomepageFeatures />
+        <Features />
       </main>
     </Layout>
   );
