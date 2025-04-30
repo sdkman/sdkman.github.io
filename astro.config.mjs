1576313c7fc2ac40b1e4af41c5158b87a9075122
Sviatoslav Makhynko <sviat.mak@gmail.com>
2025-04-30 14:54:58 +0200

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import mdx from '@astrojs/mdx';
 
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sdkman.io',
@@ -26,5 +28,6 @@ export default defineConfig({
     }),
     mdx(),
     icon(),
+    sitemap(),
   ],
 });
